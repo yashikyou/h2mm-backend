3bd763af98a181af1e844a04ca36b45f0ea78215
yashikyou <yashikyou@gmail.com>
2024-07-04 16:37:54 +0800

    init: first commit

---
+++ src/main/java/org/cc/h2mm/H2mmBackendApplication.java
@@ -0,0 +1,11 @@
+package org.cc.h2mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2mmBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(H2mmBackendApplication.class, args);
+    }
+}
